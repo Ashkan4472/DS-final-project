02e102cecd374ba47a9cbd20d1bb19996fce117b
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-01-26 21:22:06 +0330

    added search node in kdTree

--- Structures/KdTree.java
+++ Structures/KdTree.java
@@ -52,4 +52,32 @@ public class KdTree <E> {
 			return new KdNode<E>(data, coordinate[0], coordinate[1]);
 		}
 	}
+
+	/**
+	 * this method returns found node with coordinate give
+	 * <p>
+	 * this method returns found node with coordinate give if
+	 * if it wasn't found, returns null
+	 * </p>
+	 * @param coordinate
+	 * @return node in the coordinate.
+	 */
+	public KdNode<E> search(int[] coordinate) {
+		return this.searchLogic(this.node, coordinate, 0);
+	}
+
+	private KdNode<E> searchLogic(KdNode<E> node, int[] coordinate, int layer) {
+		if (node == null) {
+			return null;
+		}
+		if (node.getCoordinate()[0] == coordinate[0] && node.getCoordinate()[1] == coordinate[1]) {
+			return node;
+		}
+
+		int xORy = layer % 2;
+		if (coordinate[xORy] >= node.getCoordinate()[xORy]) {
+			return searchLogic(node.left, coordinate, layer + 1);
+		}
+		return searchLogic(node.right, coordinate, layer + 1);
+	}
 }
