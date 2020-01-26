dae74a18e0962e09699ca06c002ee750433ac412
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-01-26 20:39:23 +0330

    added coordinate to KdNode

--- Nodes/KdNode.java
+++ Nodes/KdNode.java
@@ -6,6 +6,8 @@ package Nodes;
 
 public class KdNode <E> {
 	public E data;
+	private int x;
+	private int y;
 	public KdNode<E> left;
 	public KdNode<E> right;
 
@@ -15,10 +17,15 @@ public class KdNode <E> {
 	 * this is KdNode constructor that is mostly used in KdTree
 	 * it has a data and a left and right child witch are public
 	 * </p>
+	 * 
+	 * @param x x coordinate of data
+	 * @param y y coordinate of data
 	 */
-	public KdNode() {
+	public KdNode(int x, int y) {
 		this.left = null;
 		this.right = null;
+		this.x = x;
+		this.y = y;
 	}
 
 	/**
@@ -29,11 +36,20 @@ public class KdNode <E> {
 	 * </p>
 	 * 
 	 * @param data is the data that KdNode saves inside
+	 * @param x x coordinate of data
+	 * @param y y coordinate of data
 	 */
-	public KdNode(E data) {
+	public KdNode(E data, int x, int y) {
 		this.data = data;
 		this.left = null;
 		this.right = null;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int[] getCoordinate() {
+		int[] result = {this.x, this.y};
+		return result;
 	}
 
 	@Override
