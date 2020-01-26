40ce681fc677dbf1c65eeca852b302cb94de7d4e
Ashkan Tofangdar <ashkan4472@gmail.com>
2020-01-26 21:21:38 +0330

    added addPoint to kdTree

--- Structures/KdTree.java
+++ Structures/KdTree.java
@@ -29,4 +29,27 @@ public class KdTree <E> {
 	public KdNode<E> getNode() {
 		return this.node;
 	}
+
+	/**
+	 * this method adds a point to KdTree
+	 * @param coordinate
+	 * @param data
+	 */
+	public void addPoint(int[] coordinate, E data) {
+		this.node = this.addPointLogic(node, coordinate, data, 0);
+	}
+
+	private KdNode<E> addPointLogic(KdNode<E> node, int[] coordinate, E data, int layer) {
+		if (node != null) {
+			int xORy = layer % 2;
+			if (node.getCoordinate()[xORy] >= coordinate[xORy]) {
+				node.right = addPointLogic(node.right, coordinate, data, layer + 1);
+			} else {
+				node.left = addPointLogic(node.left, coordinate, data, layer + 1);
+			}
+			return node;
+		} else {
+			return new KdNode<E>(data, coordinate[0], coordinate[1]);
+		}
+	}
 }
